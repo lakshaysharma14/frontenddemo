160e6688e25842c90e1d43e73af69d3f1b843866
[lakshay] <[laksh1013shar@gmail.com]>
2018-12-08 11:15:48 +0530

    First Commit

---
+++ demo/src/App.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+class App extends React.Component
+{
+  render()
+  {
+    return(
+        <div>
+            <Header/>
+        </div>  
+    )
+  }
+}
+
+export default App;
